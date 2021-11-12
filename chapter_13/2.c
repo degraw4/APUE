1d8d8d38979a11975145c403e54dce0c95045b88
DeGraw <60065230+degraw4@users.noreply.github.com>
2021-11-12 16:08:41 +0800

    Add files via upload

---
+++ chapter_13/2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <errno.h>
+#include <syslog.h>
+
+// syslog
+
+int main(int argc, char** argv)
+{
+    
+
+    return 0;
+}
